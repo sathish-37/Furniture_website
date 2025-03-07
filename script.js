31bfc2e58d04c061e070dd896d655aa4974aa255
sathish-37 <sathishbharathi372@gmail.com>
2025-03-07 09:48:03 +0530

    adding files and folders to git repo

---
+++ script.js
@@ -0,0 +1,11 @@
+var signsath=document.getElementById("sign-sath")
+var formcontainer=document.getElementById("form-container")
+
+// signsath.addEventListener("click", function(){
+//     formcontainer.style.display="block"
+// })
+
+function hello(event){
+    event.preventDefault()
+    formcontainer.style.display="block"
+}
